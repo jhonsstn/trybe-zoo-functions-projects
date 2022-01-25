927b5985cfea354c6a1a2e3191bbd60c5469a073
Jhon Santana <jhonsstn@gmail.com>
2022-01-25 19:14:04 -0300

    Requisito 5 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const showAllNumber = () => {
+  const animals = {};
+  species.forEach((specie) => {
+    animals[specie.name] = specie.residents.length;
+  });
+  return animals;
+};
+
+const showSpecieNumber = (animal) => {
+  let quantityOfAnimals = 0;
+  species.forEach((specie) => {
+    if (specie.name === animal.specie) {
+      quantityOfAnimals = specie.residents.length;
+    }
+  });
+  return quantityOfAnimals;
+};
+
+const showSpecieSexNumber = (animal) => {
+  let quantityOfGenderAnimals = 0;
+  const foundSpecie = species.find((specie) => specie.name === animal.specie);
+  foundSpecie.residents.forEach((resident) => {
+    if (resident.sex === animal.sex) quantityOfGenderAnimals += 1;
+  });
+  return quantityOfGenderAnimals;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return showAllNumber();
+  }
+
+  if (Object.keys(animal).length === 1) {
+    return showSpecieNumber(animal);
+  }
+
+  if (Object.keys(animal).length === 2) {
+    return showSpecieSexNumber(animal);
+  }
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  let managerEmployee;
+  employees.forEach((employee) => {
+    if (managerEmployee === undefined) {
+      managerEmployee = employee.managers.find((manager) => manager === id);
+    }
+  });
+  return managerEmployee !== undefined;
 }
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+function getRelatedEmployees(managerId) {}
 
 module.exports = { isManager, getRelatedEmployees };
