b7d185b24bc89a46d13c1a4224bd8ae63c5b1244
Jhon Santana <jhonsstn@gmail.com>
2022-01-25 19:54:25 -0300

    Requisito 6 completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) return 0;
+  if (Object.keys(entrants).length === 0) return 0;
+
+  const countedEntrants = countEntrants(entrants);
+
+  const childValue = countedEntrants.child * prices.child;
+  const adultValue = countedEntrants.adult * prices.adult;
+  const seniorValue = countedEntrants.senior * prices.senior;
+
+  return childValue + adultValue + seniorValue;
 }
 
 module.exports = { calculateEntry, countEntrants };
