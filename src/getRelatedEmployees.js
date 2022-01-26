a80df2be66026a87fd2fd5ea6e0d1067135f2d91
Jhon Santana <jhonsstn@gmail.com>
2022-01-26 13:08:24 -0300

    Requisito 4 completo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -12,6 +12,14 @@ function isManager(id) {
   return managerEmployee !== undefined;
 }
 
-function getRelatedEmployees(managerId) {}
+function getRelatedEmployees(managerId) {
+  if (isManager(managerId)) {
+    const isManagerOf = employees.filter((employee) => employee.managers.includes(managerId));
+    let nameOfEmployees = [];
+    nameOfEmployees = isManagerOf.map((employee) => `${employee.firstName} ${employee.lastName}`);
+    return nameOfEmployees;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+}
 
 module.exports = { isManager, getRelatedEmployees };
